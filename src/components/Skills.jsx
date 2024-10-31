ac9ba435fa04ec43d0fa4fac419609293c651f11
Sakila Sanharsha <sakila.sanharsha2k19@gmail.com>
2024-10-31 11:23:39 +0530

    create skill & projects section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,3 +1,5 @@
+import SkillCard from "./SkillCard";
+
 const skillItem = [
   {
     imgSrc: "/images/figma.svg",
@@ -42,7 +44,26 @@ const skillItem = [
 ];
 
 function Skills() {
-  return <section></section>;
+  return (
+    <section className="section">
+      <div className="container">
+        <h2 className="headline-2">Essential Tools I use</h2>
+
+        <p className="text-zinc-400 mt-3 mb-8 max-w-[50ch]">
+          Discover the powerful tools and technologies I use to create
+          exceptional, high-performing websites & applications.
+        </p>
+
+        <div className="grid gap-3 grid-cols-[repeat(auto-fill,_minmax(250px,1fr))]">
+          {skillItem.map(({ imgSrc, label, desc }, key) => {
+            return (
+              <SkillCard key={key} imgSrc={imgSrc} label={label} desc={desc} />
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Skills;
