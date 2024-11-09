8a1b7da6ae45755af8a1f3902165fef18686dfbc
Sakila Sanharsha <sakila.sanharsha2k19@gmail.com>
2024-11-09 15:11:18 +0530

    final

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -60,7 +60,7 @@ function Work() {
       <div className="container">
         <h2 className="headline-2 mb-8">Projects I Have Done</h2>
 
-        <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
+        <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(300px,_1fr))]">
           {works.map(({ imgSrc, title, tags, projectLink }, key) => {
             return (
               <ProjectCard
